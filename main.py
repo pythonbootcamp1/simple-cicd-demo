8cfcc4150f0e7091d4e56197c9c48fc5e5f84d17
hans <lionpythonbootcamp@gmail.com>
2025-11-19 09:12:47 +0900

    test

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import streamlit as st
 st.title("Hello World")
 
 st.write("This is a simple Streamlit app.")
+
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+# 1. 로컬에서 docker compose 실행 -> 내용 확인
+# 2. ec2 인스턴스에서 git clone 하기
+# 3. docker compose up -d -> 사이트 확인
+# 4. nginx proxy manager 에서 도메인 연결
+# 5. 로컬에서 개발하고 ec2 에서 확인하기
+# https://github.com/pythonbootcamp1/simple-cicd-demo.git
